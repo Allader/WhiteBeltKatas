056770686a89ab996e160094591bf48f94161b12
Draakoth <nikodemmajewski@gmail.com>
2024-11-11 20:00:36 +0100

    Update

--- WhiteBeltKatas3/Program.cs
+++ WhiteBeltKatas3/Program.cs
@@ -16,26 +16,26 @@ class Program
            //DRY ALERT
            if (playerLuck >= 7)
            {
-               Console.WriteLine("You found the Legendary loot!!");
+               Console.WriteLine("You found the Legendary loot!!!");
            }
            else if (playerLuck == 6)
            {
-               Console.WriteLine("You found the Epic loot!");
+               Console.WriteLine("You found the Epic loot!!");
            }
            else if (playerLuck <= 3)
            {
-               Console.WriteLine("You found the Rare loot!");
+               Console.WriteLine("You found the Rare loot");
            }
            else if (playerLuck >= 3)
            {
-               Console.WriteLine("Damn, you found garbage.");
+               Console.WriteLine("Damn, you couldn't open the chest!");
            }
        }// A LOT OF DRY BUT, i thought about making a random number appear, and depends on the number it has some "Loot" so if u get 1 u get bad loot etc. (9 && 10)
-       else if (Console.ReadLine() == "2")
+       if (Console.ReadLine() == "2")
        {
            Console.WriteLine("You ignored the chest. Good. it might be a mimic!");
        }
-       else
+       if (Console.ReadLine() == "3")
        {
            Console.WriteLine("You left the area looking for a better loot");
        }
--- WhiteBeltKatas3/obj/Debug/net8.0/WhiteBeltKatas3.AssemblyInfo.cs
+++ WhiteBeltKatas3/obj/Debug/net8.0/WhiteBeltKatas3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhiteBeltKatas3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc973ec92cf8dd1001d24de4bf47e2f1a28de28f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhiteBeltKatas3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhiteBeltKatas3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
